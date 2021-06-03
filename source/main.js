bd7496c9f96a9b6885601e8c664e3f91077570a3
aminrf95 <aminrf95@gmail.com>
2021-06-03 00:07:01 -0400

    Initial commit

---
+++ source/main.js
@@ -0,0 +1,117 @@
+class Light {
+    static lightsRemaining = 0;
+    static moveCount = 0;
+    static moveText = document.createTextNode("Moves: " + Light.moveCount);
+
+    constructor() {
+        this.box = document.createElement("div");
+        this.box.style.backgroundColor = "green";
+        this.box.style.width = "50px";
+        this.box.style.height = "50px";
+        this.box.style.margin = "10px";
+
+        this.on = true;
+        this.neighbors = [];
+
+        this.switchState = this.switchState.bind(this);
+        this.flipSwitch = this.flipSwitch.bind(this);
+        this.updateMoveCount = this.updateMoveCount.bind(this);
+        this.box.onclick = this.flipSwitch;
+    }
+
+    //Changes the state of this light only
+    switchState() {
+        if(this.on) {
+            this.box.style.backgroundColor = "gray";
+            Light.lightsRemaining--;
+        }
+        else {
+            this.box.style.backgroundColor = "green";
+            Light.lightsRemaining++;
+        }
+        this.on = !this.on;
+    }
+
+    //Changes the state of this light and all neighbors, increments moveCount
+    flipSwitch() {
+        //If game is over, do nothing
+        if(Light.lightsRemaining == 0) {return;}
+        this.switchState();
+        for(let n of this.neighbors) {
+            n.switchState();
+        }
+        this.updateMoveCount();
+    }
+
+    updateMoveCount() {
+        Light.moveCount++;
+        if(Light.lightsRemaining == 0) {
+            Light.moveText.nodeValue = "You won in " + Light.moveCount + " moves!";
+        }
+        else {
+            Light.moveText.nodeValue = "Moves: " + Light.moveCount;
+        }
+    }
+}
+
+//Creates a 3x3 grid of lights and registers all neighbors
+function makeGrid(numRows, numCols) {
+    //Initialize the grid with lights
+    let grid = [];
+    for(let i = 0; i < numRows; i++) {
+        let row = [];
+        for(let j = 0; j < numCols; j++) {
+            let gridLight = new Light();
+            row.push(gridLight);
+        }
+        grid.push(row);
+    }
+    //Register neighbors for each light
+    for(let i = 0; i < numRows; i++) {
+        for(let j = 0; j < numCols; j++) {
+            let gridLight = grid[i][j];
+            //First check for valid neighbor index, then register neighbor
+            if(i-1 >= 0) {gridLight.neighbors.push(grid[i-1][j]);}
+            if(i+1 < numRows) {gridLight.neighbors.push(grid[i+1][j]);}
+            if(j-1 >= 0) {gridLight.neighbors.push(grid[i][j-1]);}
+            if(j+1 < numCols) {gridLight.neighbors.push(grid[i][j+1]);}
+        }
+    }
+    return grid;
+}
+
+//Initializes a new game with a specified grid size,
+//and embeds it into lights_out_view.html
+function initializeGame(numRows, numCols) {
+    Light.moveCount = 0;
+    Light.moveText.nodeValue = "Moves: " + Light.moveCount;
+    Light.lightsRemaining = numRows * numCols;
+    let moveTextDiv = document.getElementById("gameMessage")
+    moveTextDiv.appendChild(Light.moveText);
+    let grid = makeGrid(numRows, numCols);
+    let gridContainer = document.getElementById("gridContainer");
+    //Remove any children
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    for(let i = 0; i < numRows; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        for(let j = 0; j < numCols; j++) {
+            row.appendChild(grid[i][j].box);
+        }
+        gridContainer.appendChild(row);
+    }
+    return gridContainer;
+}
+
+function newGame() {
+    let numRows = document.getElementById("rowSelect").value;
+    let numCols = document.getElementById("columnSelect").value;
+    initializeGame(numRows,numCols);
+}
+
+//Main script
+initializeGame(3,3);
+//End main script
